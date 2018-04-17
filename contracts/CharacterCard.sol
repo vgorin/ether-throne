441bbbe7817be4da03037524570813b56790a1d1
vgorin <vgrn.at@gmail.com>
2018-04-17 17:32:38 +0300

    basic minting and transfers (few TODOs to fix)

--- contracts/CharacterCard.sol
+++ contracts/CharacterCard.sol
@@ -75,6 +75,11 @@ contract CharacterCard {
   /// @dev ERC20 compatible field for totalSupply()
   uint16 public totalSupply;
 
+  /// @dev event names are self-explanatory:
+  /// @dev fired in mint()
+  event Minted(uint16 indexed cardId, address indexed to);
+  /// @dev fired in burn()
+  event Burnt(uint16 indexed cardId, address indexed from);
   /// @dev fired in transfer()
   event Transfer(address indexed from, address indexed to, uint16 cardId);
 
@@ -127,4 +132,84 @@ contract CharacterCard {
     return owner;
   }
 
+  /**
+   * @dev Creates new card with `cardId` ID specified and
+   * assigns to an address `to` an ownership of that card.
+   * @param cardId ID of the card to create
+   * @param to an address to assign created card ownership to
+   */
+  function mint(uint16 cardId, address to) public {
+    // TODO: check that sender has permissions to mint a card
+    // validate destination address
+    require(to != address(0));
+
+    // check if card doesn't exist
+    require(!exists(cardId));
+
+    // create a new card in memory
+    Card memory card = Card({
+      creationTime: uint32(block.number),
+      ownershipModified: 0,
+      attributesModified: 0,
+      gamesPlayed: 0,
+      wins: 0,
+      loses: 0,
+      state: 0,
+      rarity: 0, // TODO: generate rarity
+      lastGamePlayed: 0,
+      attributes: 0, // TODO: set attributes according to rarity
+      owner: to
+    });
+
+    // persist card to the storage
+    cards[cardId] = card;
+
+    // update new owner balance
+    balances[to]++;
+
+    // update total supply
+    totalSupply++;
+
+    // fire an event
+    Minted(cardId, to);
+  }
+
+  /**
+   * @notice Transfers ownership rights of a card defined
+   * by the `cardId` to a new owner specified by address `to`
+   * @dev Requires the sender of the transaction to be an owner
+   * of the card specified (`cardId`)
+   * @param to new owner address
+   * @param cardId ID of the card to transfer ownership rights for
+   */
+  function transfer(address to, uint16 cardId) public {
+    // call sender gracefully - `from`
+    address from = msg.sender;
+
+    // validate destination address
+    require(to != address(0));
+    require(to != from);
+
+    // get the card from the storage
+    // TODO: check if modifying card in the storage is cheaper
+    Card memory card = cards[cardId];
+
+    // validate card ownership (and existence)
+    require(card.owner == from);
+
+    // update ownership modified time
+    card.ownershipModified = uint32(block.number);
+    // update ownership itself
+    card.owner = to;
+
+    // persist card back into the storage
+    cards[cardId] = card;
+
+    // update new and old owner balances
+    balances[from]--;
+    balances[to]++;
+
+    // fire an event
+    Transfer(from, to, cardId);
+  }
 }
--- test/character_card.js
+++ test/character_card.js
@@ -5,10 +5,51 @@ contract('CharacterCard', function(accounts) {
 		const card = await CharacterCard.new();
 		assert.equal(0, await card.totalSupply(), "initial totalSupply must be zero");
 		assert.equal(0, await card.balanceOf(accounts[0]), "initial card balance must be zero");
-		assert(!await card.exists(0x1), "card 1 should not exist initially");
+		assert(!await card.exists(0x1), "card 0x1 should not exist initially");
 		await assertThrowsAsync(async function() {await card.ownerOf(0x1);});
 	});
-	
+
+	it("mint: mint a card", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[0]);
+		assert.equal(1, await card.totalSupply(), "totalSupply after minting a card must be 1");
+		assert.equal(1, await card.balanceOf(accounts[0]), "card balance after minting a card must be 1");
+		assert(await card.exists(0x1), "card 0x1 should exist after minting");
+		assert.equal(accounts[0], await card.ownerOf(0x1), "card 0x1 has wrong owner after minting it to " + accounts[0]);
+	});
+	it("mint: impossible to mint the same card twice", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[0]);
+		await assertThrowsAsync(async function() {await card.mint(0x1, accounts[0])});
+	});
+	it("mint: impossible to mint a card to a zero address", async function() {
+		const card = await CharacterCard.new();
+		await assertThrowsAsync(async function() {await card.mint(0x1, 0)});
+	});
+
+	it("transfer: transfer a card", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[0]);
+		await card.transfer(accounts[1], 0x1);
+		assert.equal(0, await card.balanceOf(accounts[0]), "sender's card balance after transferring a card must be 0");
+		assert.equal(1, await card.balanceOf(accounts[1]), "receiver's card balance after transferring a card must be 1");
+		assert.equal(accounts[1], await card.ownerOf(0x1), "card 0x1 has wrong owner after transferring it to " + accounts[1]);
+	});
+	it("transfer: impossible to transfer a card which you do not own", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[1]);
+		await assertThrowsAsync(async function() {await card.transfer(accounts[2], 0x1);});
+	});
+	it("transfer: impossible to transfer a card to a zero address", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[0]);
+		await assertThrowsAsync(async function() {await card.transfer(0, 0x1);});
+	});
+	it("transfer: impossible to transfer a card to oneself", async function() {
+		const card = await CharacterCard.new();
+		await card.mint(0x1, accounts[0]);
+		await assertThrowsAsync(async function() {await card.transfer(accounts[0], 0x1);});
+	});
 });
 
 async function assertThrowsAsync(fn) {
