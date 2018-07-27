04bc4ded3b08db0ea530a9d64754a2a0876fa188
vgorin <vgrn.at@gmail.com>
2018-07-27 12:03:33 +0300

    loom configuration for truffle

--- truffle.js
+++ truffle.js
@@ -1,8 +1,25 @@
+/* START: Loom Network Configuration */
+const { readFileSync } = require('fs');
+const LoomTruffleProvider = require('loom-truffle-provider');
+
+const chainId    = 'default';
+const writeUrl   = 'http://127.0.0.1:46658/rpc';
+const readUrl    = 'http://127.0.0.1:46658/query';
+const privateKey = readFileSync('./private_key', 'utf-8');
+
+const loomTruffleProvider = new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+
+/* END: Loom Network Configuration */
+
 // npm install truffle-hdwallet-provider
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
 	networks: {
+		loom_dapp_chain: {
+			provider: loomTruffleProvider,
+			network_id: '*'
+		},
 		development: {
 			provider: new HDWalletProvider(
 				"12 mimic words",
